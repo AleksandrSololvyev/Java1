414682bc280fea70ceff9541ab3c6c07610cbf63
Aleksandr_Solovyev <solovyev-1-aleksandr@mail.ru>
2022-12-15 21:02:51 +0300

    lesson5

---
+++ JS/lesson5/task4.java
@@ -0,0 +1,25 @@
+package lesson5;
+import java.util.*;
+public class task4 {
+    public static void main(String[] args) {
+        String[][] boards = new String[8][8];
+        boards[3][0] = "Q";
+        boards[5][1] = "Q";
+        boards[7][2] = "Q";
+        boards[1][3] = "Q";
+        boards[6][4] = "Q";
+        boards[0][5] = "Q";
+        boards[2][6] = "Q";
+        boards[4][7] = "Q";
+
+        for (int i = 0; i < boards.length; i++) {
+			for (int j = 0; j < boards[i].length; j++) {
+                if (boards[i][j] == null) {
+                    boards[i][j] = "X";
+                }
+				System.out.print(boards[i][j] + "\t");
+			}
+			System.out.println();
+		}
+}
+}
